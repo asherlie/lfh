dc1a4518b2e8f394cf50a95db90a27f0c2579977
asherlie <lieber.asher@gmail.com>
2024-01-28 18:04:14 -0500

    implemented print_##name

--- test.c
+++ test.c
@@ -8,18 +8,27 @@ uint16_t hfunc_ptr(int* x) {
     return (x) ? *x : 0;
 }
 
+uint16_t nwfunc(char* x) {
+    return (x) ? *x : 0;
+}
+
 INIT_LFH(int, int, ashmap)
 INIT_LFH(int*, int, ashmap_ptr)
+INIT_LFH(int*, int*, ptrptr)
+INIT_LFH(char*, float, networth)
 
 int main(){
 
     ashmap m;
     ashmap_ptr mp;
-    int x = 9;
+    ptrptr pp;
+    networth net;
 
     _Bool found;
     init_ashmap(&m, 100, hfunc);
     init_ashmap_ptr(&mp, 100, hfunc_ptr);
+    init_ptrptr(&pp, 100, hfunc_ptr);
+    init_networth(&net, 100, nwfunc);
 
     for (int i = 0; i < 1000; ++i) {
         insert_ashmap(&m, 100, i);
@@ -28,5 +37,11 @@ int main(){
     printf("ashmap: %i\n", lookup_ashmap(&m, 100, &found));
 
     insert_ashmap_ptr(&mp, NULL, 90);
-    printf("ashmap_ptr: %i\n", lookup_ashmap_ptr(&mp, &x, &found));
+    printf("ashmap_ptr: %i\n", lookup_ashmap_ptr(&mp, NULL, &found));
+
+    insert_ptrptr(&pp, NULL, NULL);
+    lookup_ptrptr(&pp, NULL, &found);
+
+    insert_networth(&net, "john", 99.3);
+    print_networth(&net, "\"%s\": %f");
 }
